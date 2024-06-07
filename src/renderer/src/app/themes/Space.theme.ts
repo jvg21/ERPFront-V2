14fc2e4e4ebad19b203b2d444ec107ce9672271e
Bruno Guttervill <bguttervill@gmail.com>
2024-06-07 18:59:07 -0300

    themes

---
+++ src/renderer/src/app/themes/Space.theme.ts
@@ -0,0 +1,21 @@
+import { ThemeType } from "../../@types/ThemeType";
+
+export const SpaceTheme: ThemeType = {
+    name: "Space",
+    style: {
+        body: '#000000',
+        background: '#000000',
+        secondaryBackground: '#111111',
+        text: '#FFFFFF',
+        secondaryText: '#BDBDBD',
+        accent: '#FFFFFF',
+        secondaryAccent: '#FFFFFF',
+        error: '#FF5252',
+        warning: '#FFD600',
+        info: '#03A9F4',
+        success: '#4CAF50',
+        white: '#FFFFFF',
+        black: '#000000',
+        shadow: 'rgba(255, 255, 255, 0.1)'
+    }
+}
